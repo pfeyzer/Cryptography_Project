49de92df4ac6177fb3c752a475de2d8000a02e4f
pfeyzer <eyad.ebrahem@student.giu-uni.de>
2025-12-10 19:08:20 +0200

    secret sharing + auction part

--- auction.py
+++ auction.py
@@ -1,3 +1,27 @@
-def register_bidder():
+from commitments import commit_bid, verify_commitment
+from zkproofs import generate_range_proof, verify_range_proof
+from secretsharing import share_value
+
+def register_bidder(bidder_id, bid, prime, max_bid, servers):
+    # commitment
+    commitment, randomness = commit_bid(bid)
+
+    # range proof
+    proof = generate_range_proof(bid, randomness, max_bid)
+
+    # verify proof before accepting
+    if not verify_range_proof(commitment, proof):
+        print("Bidder", bidder_id, "rejected: invalid range proof")
+        return false
+
+    # secret sharing for the bid
+    share1, share2, share3 = share_value(bid, prime)
+
+    # send shares to servers
+    servers[0].receive_share(bidder_id, share1)
+    servers[1].receive_share(bidder_id, share2)
+    servers[2].receive_share(bidder_id, share3)
+
+    return True
 
 def run_auction():
--- secret_sharing.py
+++ secret_sharing.py
@@ -1,3 +1,10 @@
-def share_value():
+import secrets
 
-def reconstruct():
+def share_value(bid, prime):
+    share1 = secrets.randbelow(prime)
+    share2 = secrets.randbelow(prime)
+    share3 = (bid - share1 - share2) % prime
+    return share1, share2, share3
+
+def reconstruct(shares, prime):
+    return sum(shares)% prime
