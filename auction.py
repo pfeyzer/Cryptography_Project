5c64cf0d6694c57cc8244f70984339b740025a50
mhmdamgd1 <4jd8ett0@proton.me>
2025-12-10 19:43:49 +0200

    mhmd work

--- auction.py
+++ auction.py
@@ -1,3 +1,40 @@
-def register_bidder():
+class SimpleAuctionOrchestrator:
+    def __init__(self, p=p, min_bid=100, max_bid=1000):
+        self.p = p
+        self.min_bid = min_bid
+        self.max_bid = max_bid
+        self.bidders = []
+        self.server_storage = {1: {}, 2: {}, 3: {}}
 
-def run_auction():
+
+    
+
+
+    def run_auction(self):
+        winner_id, winning_bid = mpc_argmax(self.bidders, self.p)
+        return winner_id, winning_bid
+
+
+if __name__ == "__main__":
+    orch = SimpleAuctionOrchestrator()
+    bids = [150, 920, 600]
+    for i, b in enumerate(bids, start=1):
+        shares = share_value(b)
+        orch.register_bidder(i, b, shares)
+        print("Example 1:", orch.run_auction())
+
+
+    orch = SimpleAuctionOrchestrator()
+    bids = [350, 350, 100, 300]
+    for i, b in enumerate(bids, start=1):
+        shares = share_value(b)
+        orch.register_bidder(i, b, shares)
+        print("Example 2:", orch.run_auction())
+
+
+    orch = SimpleAuctionOrchestrator()
+    bids = [10, 999, 300, 700, 2000]
+    for i, b in enumerate(bids, start=1):
+        shares = share_value(b)
+        orch.register_bidder(i, b, shares)
+        print("Example 3:", orch.run_auction())
--- mpc.py
+++ mpc.py
@@ -1,9 +1,70 @@
-def mpc_add():
+import random
 
-def mpc_sub():
 
-def mpc_mult():
+p = 2**127 - 1
 
-def mpc_compare():
 
-def mpc_argmax():
+def share_value(x, p=p):
+    s1 = random.randrange(p)
+    s2 = random.randrange(p)
+    s3 = (x - s1 - s2) % p
+    return s1, s2, s3
+
+
+def reconstruct(shares, p=p):
+    return sum(shares) % p
+
+
+def signed(x, p=p):
+    return x if x <= p//2 else x - p
+
+
+def mpc_add(a, b, p=p):
+    return [(a[i] + b[i]) % p for i in range(3)]
+
+
+def mpc_sub(a, b, p=p):
+    return [(a[i] - b[i]) % p for i in range(3)]
+
+
+def mpc_mult(a, b, p=p):
+    A = reconstruct(a, p)
+    B = reconstruct(b, p)
+    prod = (A * B) % p
+    return share_value(prod, p)
+
+
+def compute_difference_share(ai, bi, p=p):
+    return (ai - bi) % p
+
+
+def reconstruct_difference(diffs, p=p):
+    return sum(diffs) % p
+
+
+def compare(a, b, p=p):
+    diffs = []
+    for i in range(3):
+        diffs.append(compute_difference_share(a[i], b[i], p))
+        D = reconstruct_difference(diffs, p)
+        D_signed = signed(D, p)
+    if D_signed > 0:
+        return 1
+    elif D_signed < 0:
+        return -1
+    else:
+        return 0
+
+
+def mpc_argmax(bidders, p=p):
+    if not bidders:
+        return None, None
+    current_id = bidders[0][0]
+    current_shares = bidders[0][1]
+    for bidder_id, shares in bidders[1:]:
+        c = compare(shares, current_shares, p)
+    if c == 1:
+        current_id = bidder_id
+        current_shares = shares
+        winning_bid = reconstruct(current_shares, p)
+        return current_id, winning_bid
